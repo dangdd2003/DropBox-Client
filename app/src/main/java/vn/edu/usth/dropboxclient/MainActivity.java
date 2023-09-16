197296e8ea5ab072950efd46b994db26309eac8f
bi12-040 <anhtn.bi12-040@st.usth.edu.vn>
2023-09-16 11:02:10 +0700

    finish photos fragment

--- app/src/main/java/vn/edu/usth/dropboxclient/MainActivity.java
+++ app/src/main/java/vn/edu/usth/dropboxclient/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import vn.edu.usth.dropboxclient.fragments.PhotosFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+}}
