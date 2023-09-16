790c75e726668563a9f91edb9d66cfe701ee8dfd
Vivarium69420 <vivarium69420@gmail.com>
2023-09-16 11:25:11 +0700

    adding toolbar

--- app/src/main/java/vn/edu/usth/dropboxclient/MainActivity.java
+++ app/src/main/java/vn/edu/usth/dropboxclient/MainActivity.java
@@ -1,14 +1,28 @@
 package vn.edu.usth.dropboxclient;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
     }
 }
