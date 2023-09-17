e755aa5c54cb0b6efec73f10233addb28b9b4574
Vivarium69420 <vivarium69420@gmail.com>
2023-09-17 16:32:15 +0700

    fxing layout

--- app/src/main/java/vn/edu/usth/dropbox/ui/settings/SettingsFragment.java
+++ app/src/main/java/vn/edu/usth/dropbox/ui/settings/SettingsFragment.java
@@ -2,8 +2,11 @@ package vn.edu.usth.dropbox.ui.settings;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +23,11 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        return root;
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
     }
 }
