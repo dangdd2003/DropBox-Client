dfc27ba499c3b194d6219189b6339fbf50e23831
dangdd2003 <dangdoan2206@gmail.com>
2023-10-05 02:52:11 +0700

    fix login activity UI and functionality

--- app/src/main/java/vn/edu/usth/dropbox/Login.java
+++ app/src/main/java/vn/edu/usth/dropbox/Login.java
@@ -4,28 +4,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
 
-public class Login extends AppCompatActivity {
+import vn.edu.usth.dropbox.databinding.ActivityLoginBinding;
+
+public class Login extends AppCompatActivity implements View.OnClickListener{
+
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        Button loginButton = findViewById(R.id.continue_btn);
-        TextInputEditText email = findViewById(R.id.email);
+        binding.continueBtn.setOnClickListener(this);
+        binding.loginWithGoogle.setOnClickListener(this);
+        binding.email.setOnClickListener(this);
+        binding.troubleBtn.setOnClickListener(this);
+    }
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    @Override
+    public void onClick(View view) {
+        binding.continueBtn.setOnClickListener(v -> {
+            if (binding.email.getText().toString().equals("alo1234")) {
                 Intent intent = new Intent(Login.this, MainActivity.class);
                 startActivity(intent);
-
+            } else {
+                Toast.makeText(this, "???????????", Toast.LENGTH_SHORT).show();
             }
+        });
+
+        binding.loginWithGoogle.setOnClickListener(v -> {
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+        });
+        
+        binding.troubleBtn.setOnClickListener(v -> {
+            Toast.makeText(this, "Who care ? ", Toast.LENGTH_SHORT).show();
         });
     }
 }
